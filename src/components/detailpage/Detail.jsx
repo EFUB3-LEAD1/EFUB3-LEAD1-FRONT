b6c0aecdd5b16414337c48fb4d3f9e619b27c528
hanby-choi <hanby.choi@ewhain.net>
2023-05-18 00:42:43 +0900

    feat: 상세 페이지 퍼블리싱중

--- src/components/detailpage/Detail.jsx
+++ src/components/detailpage/Detail.jsx
@@ -1,22 +1,38 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router';
-import NavigationBar from '../_common/NavigationBar';
 import { S } from './Detail.style';
 import back_arrow from '../../assets/detailpage/back_arrow.svg';
+import verified from '../../assets/detailpage/verified.svg';
 
 const Detail = props => {
     const nav = useNavigate();
     // info[id].title
     return (
         <S.Wrapper>
-            <S.InfoImage>
-                <img src={props.info.infoImage.infoImage} />
-            </S.InfoImage>
+            <S.InfoContainer>
+                <S.InfoImage src={props.info.infoImage.infoImage} />
+            </S.InfoContainer>
             <S.BackBtn onClick={() => nav('/whole-package')}>
                 <img src={back_arrow} width={25} />
             </S.BackBtn>
-            <NavigationBar />
+            <S.InfoTop>
+                <img src={verified} />
+                <div className='leftText'>약속한 여행! 100% 보상!</div>
+                <div className='rightText'>개런티 프로그램 적용 상품</div>
+            </S.InfoTop>
+            <S.ItemContainer>
+                <S.ItemNum>{props.info.itemNum}</S.ItemNum>
+                <S.DetailContainer>
+                    <div className='tagContainer'>
+                        <div className='standard'>스탠다드</div>
+                        <div className='depart'>출발 예정</div>
+                    </div>
+                    <div className='info'>여행상품 핵심 정보</div>
+                </S.DetailContainer>
+                <S.Title>{props.info.title}</S.Title>
+                <S.HashTag>{props.info.hashtag}</S.HashTag>
+                <S.Explanation>{props.info.deal}</S.Explanation>
+            </S.ItemContainer>
         </S.Wrapper>
     );
 };
--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Detail from '../components/detailpage/Detail';
-import infoImage from '../assets/detailpage/InfoImage.png';
+import infoImage from '../assets/detailpage/info_image_big.png';
 import { useParams } from 'react-router-dom';
 
 const DetailPage = () => {
@@ -10,22 +10,24 @@ const DetailPage = () => {
             id: 1,
             infoImage: { infoImage },
             title: '서유럽 4국 10일',
+            itemNum: 'EW131230603LHT',
             hashtag:
-                '#시내호텔 2박 # 융프라우요흐 # 에펠탑 전망대 # 세느강유람선 # 베네치아 대운하투어 # 런던',
+                '#시내호텔 2박 #융프라우요흐 #에펠탑 전망대 #세느강유람선 #베네치아 대운하투어 #런던',
             explanation: '단 일주일간 40만원 할인',
             price: '4,499,000 원 ~',
             originalPrice: '4,899,000원',
-            deal: 0,
+            deal: '금주핫딜',
         },
         {
             id: 2,
             infoImage: { infoImage },
             title: '서유럽 4국 10일',
+            itemNum: 'EW131230603LHT',
             hashtag: '#시내호텔 2박 # 융프라우요흐 # 에펠탑 전망대',
             explanation: '단 일주일간 40만원 할인',
             price: '4,499,000 원 ~',
             originalPrice: '4,899,000원',
-            deal: 0,
+            deal: '금주핫딜',
         },
     ];
     return <Detail info={info[id - 1]} />;
