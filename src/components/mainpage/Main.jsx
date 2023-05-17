118a358aa664bc956524e8481908924930a2471b
서진 <dltjwls0529@gmail.com>
2023-05-17 22:37:58 +0900

    feat: 메인 페이지 퍼블리싱

--- src/components/mainpage/Main.jsx
+++ src/components/mainpage/Main.jsx
@@ -1,23 +1,134 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router';
 import NavigationBar from '../_common/NavigationBar';
-
 import { S } from './Main.style';
+
 import logo from '../../assets/mainpage/logo.png';
 import { FaRegUserCircle } from 'react-icons/fa';
+import mainimg1 from '../../assets/mainpage/mainimg1.png';
+import mainimg2 from '../../assets/mainpage/mainimg2.png';
+import flight from '../../assets/mainpage/flight.svg';
+import jeju from '../../assets/mainpage/jeju.png';
+import golf from '../../assets/mainpage/golf.svg';
+import honeymoon from '../../assets/mainpage/honeymoon.svg';
+import koreaflight from '../../assets/mainpage/korea_flight.svg';
+import sale from '../../assets/mainpage/sale.svg';
+import worldmap from '../../assets/mainpage/worldmap.svg';
+import place from '../../assets/mainpage/place.svg';
+import calendar from '../../assets/mainpage/calendar.svg';
+import thumbup from '../../assets/mainpage/thumbup.svg';
 
 const Main = () => {
     const nav = useNavigate();
+    const [isSplash, setIsSplash] = useState(true);
+    useEffect(() => {
+        setTimeout(function () {
+            setIsSplash(false);
+        }, 1500);
+    }, []);
     return (
         <S.Wrapper>
-            <S.TopBar>
-                <S.Logo src={logo} />
-                <S.MyPageBtn onClick={() => nav('/mypage')}>
-                    <FaRegUserCircle size='25' />
-                    <div className='text'>마이페이지</div>
-                </S.MyPageBtn>
-            </S.TopBar>
-            <NavigationBar />
+            {isSplash ? (
+                <S.SplashContainer>
+                    <S.SplashLogo src={logo} />
+                </S.SplashContainer>
+            ) : (
+                <>
+                    <S.TopBar>
+                        <S.Logo src={logo} />
+                        <S.MyPageBtn onClick={() => nav('/mypage')}>
+                            <FaRegUserCircle size='25' />
+                            <div className='text'>마이페이지</div>
+                        </S.MyPageBtn>
+                    </S.TopBar>
+                    <S.MainText>원하시는 패키지를 선택해주세요!</S.MainText>
+                    <S.ImgContainer>
+                        <div
+                            className='inner'
+                            onClick={() => nav('/whole-package')}
+                        >
+                            <img src={mainimg1} />
+                            <div className='text'>전체 패키지 전체보기</div>
+                        </div>
+                        <div className='inner'>
+                            <img src={mainimg2} />
+                            <div className='text'>맞춤 패키지 전체보기</div>
+                        </div>
+                    </S.ImgContainer>
+                    <S.SubText>
+                        원하면,&nbsp;
+                        <span style={{ color: 'var(--purple1)' }}>
+                            어디든 완벽하게!
+                        </span>
+                    </S.SubText>
+                    <S.IconContainer>
+                        <div className='imgdiv'>
+                            <img className='img1' src={flight} />
+                            <div className='imgtext'>해외여행</div>
+                        </div>
+                        <div className='imgdiv'>
+                            <img className='img2' src={jeju} />
+                            <div className='imgtext'>제주/국내</div>
+                        </div>
+                        <div className='imgdiv'>
+                            <img className='img3' src={golf} />
+                            <div className='imgtext'>골프</div>
+                        </div>
+                        <div className='imgdiv'>
+                            <img className='img4' src={honeymoon} />
+                            <div className='imgtext'>허니문</div>
+                        </div>
+                        <div className='imgdiv'>
+                            <img className='img5' src={koreaflight} />
+                            <div className='imgtext'>지방출발</div>
+                        </div>
+                    </S.IconContainer>
+                    <S.MenuContainer>
+                        <S.MenuDiv className='left-top'>
+                            <div className='text'>항공</div>
+                            <S.Sale src={sale} />
+                        </S.MenuDiv>
+                        <S.MenuDiv>
+                            <div className='text'>해외호텔</div>
+                        </S.MenuDiv>
+                        <S.MenuDiv className='right-top'>
+                            <div className='text'>국내숙박</div>
+                        </S.MenuDiv>
+                        <S.MenuDiv className='left-bottom'>
+                            <div className='text'>렌터카</div>
+                        </S.MenuDiv>
+                        <S.MenuDiv>
+                            <div className='text'>에어/투어텔</div>
+                        </S.MenuDiv>
+                        <S.MenuDiv className='right-bottom'>
+                            <div className='text'>투어/입장권</div>
+                        </S.MenuDiv>
+                    </S.MenuContainer>
+                    <S.ButtonContainer>
+                        <div className='imgdiv'>
+                            <img src={worldmap} />
+                            <div className='imgtext'>해외여행정보</div>
+                            <div className='subtext'>국가별 안내</div>
+                        </div>
+                        <div className='imgdiv'>
+                            <img src={place} />
+                            <div className='imgtext'>도시 정보</div>
+                            <div className='subtext'>플레이스</div>
+                        </div>
+                        <div className='imgdiv'>
+                            <img src={calendar} />
+                            <div className='imgtext'>일정 생성</div>
+                            <div className='subtext'>플래너</div>
+                        </div>
+                        <div className='imgdiv'>
+                            <img src={thumbup} />
+                            <div className='imgtext'>SAFETY&JOY</div>
+                            <div className='subtext'>안심여행 지원</div>
+                        </div>
+                    </S.ButtonContainer>
+                    <NavigationBar />
+                </>
+            )}
         </S.Wrapper>
     );
 };
--- src/components/mainpage/Main.style.js
+++ src/components/mainpage/Main.style.js
@@ -4,6 +4,17 @@ const S = {};
 S.Wrapper = styled.div`
     display: flex;
     flex-direction: column;
+    padding-bottom: 130px;
+`;
+S.SplashContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+S.SplashLogo = styled.img`
+    width: 40%;
 `;
 S.TopBar = styled.div`
     display: flex;
@@ -25,6 +36,136 @@ S.MyPageBtn = styled.div`
         font-size: 0.6rem;
         font-weight: 400;
         margin-top: 5px;
+    }
+`;
+S.MainText = styled.div`
+    margin: 10px 0 20px 0;
+    text-align: center;
+    color: var(--purple1);
+    font-weight: 600;
+    font-size: 1rem;
+`;
+S.ImgContainer = styled.div`
+    width: 100%;
+    display: flex;
+    .inner {
+        width: 50%;
+        height: 35vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+    .text {
+        width: 33%;
+        position: absolute;
+        color: var(--white);
+        font-size: 1.2rem;
+        font-weight: 600;
+        text-align: center;
+        word-break: keep-all;
+        text-shadow: 0px 0px 5px rgba(0, 0, 0, 0.7);
+    }
+`;
+S.SubText = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    padding: 15px 0 25px 25px;
+    font-size: 0.9rem;
+    font-weight: 600;
+`;
+S.IconContainer = styled.div`
+    padding: 0 10px;
+    display: flex;
+    justify-content: space-between;
+    .imgdiv {
+        width: 20%;
+        height: 52px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        position: relative;
+    }
+    .img2 {
+        width: 30%;
+    }
+    .img3 {
+        margin-left: 3px;
+        width: 40%;
+    }
+    .img4 {
+        width: 35%;
+    }
+    .imgtext {
+        position: absolute;
+        bottom: 0;
+        font-size: 0.7rem;
+        font-weight: 600;
+    }
+`;
+S.MenuContainer = styled.div`
+    margin: 30px 20px 10px 20px;
+    height: 110px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    .left-top {
+        border-radius: 10px 0 0 0;
+    }
+    .right-top {
+        border-radius: 0 10px 0 0;
+    }
+    .right-bottom {
+        border-radius: 0 0 10px 0;
+    }
+    .left-bottom {
+        border-radius: 0 0 0 10px;
+    }
+`;
+S.Sale = styled.img`
+    width: 8%;
+    position: absolute;
+    margin-bottom: 35px;
+    margin-left: 60px;
+`;
+S.MenuDiv = styled.div`
+    background-color: var(--purple3);
+    width: 32.8%;
+    margin-bottom: 0.9%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .text {
+        font-size: 0.9rem;
+        font-weight: 600;
+    }
+`;
+S.ButtonContainer = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+    .imgdiv {
+        width: 25%;
+        height: 50px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        position: relative;
+    }
+    .imgtext {
+        margin-top: 6px;
+        font-size: 0.7rem;
+        font-weight: 600;
+    }
+    .subtext {
+        margin-top: 2px;
+        font-size: 0.6rem;
+        font-weight: 300;
+        color: var(--gray);
     }
 `;
 
